82137acc5bd9a24fa1de23b4b02550d72864b714
B. Hrnja <b@hrnja.com>
2024-09-22 23:52:16 -0400

    Calculator fully functional
    Wrote every function needed to create a functional calculator including: a function that appends values, functions that delete the previous value and that clear the display. And lastly, a function that evaluates the values in the display.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,24 @@
 const calcDisplay = document.getElementById("calcDisplay")
+
+// Add button input to the display input.
+function displayInput(input) {
+    calcDisplay.value += input;
+}
+
+function clearAll() {
+    calcDisplay.value = "";
+}
+
+// This almost works, but it deletes all and changes the input to 1.
+function deleteFrom() {
+    calcDisplay.value = calcDisplay.value.slice(0,-1);
+}
+
+function evalWith () {
+    try {
+        calcDisplay.value = eval(calcDisplay.value);
+    }
+    catch(error) {
+        calcDisplay.value = "Error";
+    }
+}
